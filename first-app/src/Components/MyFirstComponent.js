54466ef409c4c366f35aacace80a39bb121acf05
Isaamarod <35488726+Isaamarod@users.noreply.github.com>
2020-07-15 14:14:08 +0200

    Change component

--- first-app/src/Components/MyFirstComponent.js
+++ first-app/src/Components/MyFirstComponent.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-class MyFirstComponent extends React.Component{
+class MyFirstComponent extends Component{
 
     render(){
+        let libro = {
+            nombre: 'El dia que se perdio la Cordura',
+            personajes: ['jon','Kate','Mery'],
+            num_pags: 400
+            
+        }
         return(
-            <h1>Hi, I'm first component</h1>
+            /** o usar un div */
+            <React.Fragment> 
+                <h1>Hi, I'm first component</h1>
+                <h3>{'Los personajes del libro '+ libro.nombre+ ' son: '+libro.personajes}</h3>
+
+                <ol>
+                    {
+                        libro.personajes.map((personajes,i) => {
+                            console.log(personajes)
+                            return(
+                                <li key="i">
+                                    {personajes}
+                                </li>
+                            );
+                        }
+                        )
+                    }
+                </ol>
+                
+            </React.Fragment>
+            
         )
     }
 

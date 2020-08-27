617debfe900738e6cbd2b991cdbe9f1e3105dbe1
Isaamarod <35488726+Isaamarod@users.noreply.github.com>
2020-08-27 17:47:38 +0200

    hello world functional component

---
+++ counter-app/src/PrimeraApp.js
@@ -0,0 +1,11 @@
+//Los componentes tienen la primera letra capitalizada
+//I'll work in FUNCTIONAL COMPONENTS
+ import React from 'react';
+const PrimeraApp = () =>{
+
+    return <h1>Hola Mundo</h1>;
+
+ }
+
+
+export default PrimeraApp;
